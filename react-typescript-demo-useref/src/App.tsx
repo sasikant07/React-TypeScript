9b5bbc078fa752540bf0d04bcb441f1c4682d2cd
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-09-12 22:22:17 +0530

    types for useRef hooks

---
+++ react-typescript-demo-useref/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MutableRef from './components/ref/MutableRef';
+
+function App() {
+  return (
+    <div className="App">
+      <MutableRef />
+    </div>
+  );
+}
+
+export default App;

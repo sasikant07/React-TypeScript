dbef83ba4b751b4f8bb3452c7c61b3cd1c8442d3
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-09-20 21:41:52 +0530

    Added techniques for extracting component prop types

---
+++ react-typescript-demo/src/components/CustomComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Greet from './Greet'
+
+const CustomComponent = (props: React.ComponentProps<typeof Greet>) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+export default CustomComponent
